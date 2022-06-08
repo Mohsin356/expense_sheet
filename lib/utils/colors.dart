a5825cb064aa1d4a8eda6d6f6e6bcf2195470ea3
Mohsin.Raza <Mohsin356>
2022-06-08 19:33:42 +0500

    List of expenses

---
+++ lib/utils/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+class UiColors{
+  static const priceBorderClr = Colors.black;
+}
--- lib/views/screens/homeScreen.dart
+++ lib/views/screens/homeScreen.dart
@@ -1,6 +1,7 @@
 import 'package:expense_sheet/controllers/transactionController.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:expense_sheet/utils/colors.dart';
 
 class HomePage extends StatelessWidget {
    HomePage({Key? key}) : super(key: key);
@@ -22,7 +23,23 @@ class HomePage extends StatelessWidget {
                  transactions.transactionsList.map((tx){
                    return Card(
                      elevation: 5,
-                     child: Text(tx.title!),
+                     child: Row(
+                       children: [
+                         Container(
+                           margin: const EdgeInsets.symmetric(vertical:10,horizontal: 15),
+                           decoration: BoxDecoration(
+                             border: Border.all(color: UiColors.priceBorderClr,width: 2)
+                           ),
+                           padding: const EdgeInsets.all(10),
+                           child: Text("${tx.amount.toString()} Rs")),
+                         Column(
+                           children: [
+                             Text(tx.title.toString()),
+                             Text(tx.date.toString()),
+                           ],
+                         )
+                       ],
+                     )
                    );
                  }).toList(),
              )
